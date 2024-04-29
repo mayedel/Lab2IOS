695e13beedb82f267ccc22b7773565506b66135a
María Espejo <mayed92@hotmail.com>
2024-04-29 16:42:18 +0200

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by María Espejo on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
